f340f6fcbacc3586eb103b08d255ea477f6d0a98
Eddie Warner <happyspace@gmail.com>
2016-06-05 17:54:02 -0700

    Create a standard maven project. Add standard testing frameworks.

---
+++ src/test/java/com/happyspace/pdchallenge/AppTest.java
@@ -0,0 +1,26 @@
+package com.happyspace.pdchallenge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Unit test for simple App.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class AppTest
+{
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
